9fda8992db7f8c03192a00b3e05d07cf1ef2c0d9
Greg Patrick <greg.patrick@fullsendsimsport.com.au>
2026-04-16 14:13:20 +0930

    Revert "Remove hash from npm build creation naming"
    This reverts commit b45457943bc1270dbc61cbbc601426e08ff919fe.

--- vite.config.js
+++ vite.config.js
@@ -4,24 +4,12 @@ import path from "path"
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        // This removes the hash from the main JS file
-        entryFileNames: `assets/[name].js`,
-        // This removes the hash from chunk files
-        chunkFileNames: `assets/[name].js`,
-        // This removes the hash from CSS and other assets
-        assetFileNames: `assets/[name].[ext]`
-      }
-    }
-  }, // <--- This was the missing brace
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
   css: {
-    transformer: 'postcss',
+    transformer: 'postcss', // This forces it to use Tailwind v3 rules
   }
 })
